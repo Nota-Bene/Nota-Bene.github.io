969540fb4a62796bfb88defa5b177826fb74805a
nina-werklaptop <n.becx@flexpulse.nl>
2020-12-24 15:32:18 +0000

    Updates

---
+++ component---src-pages-cv-js-537752ac83005e1915de.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"4ngj":function(n,e,t){"use strict";t.r(e);t("Wbzz");var a=t("q1tI"),o=t.n(a),r=t("vrFN");e.default=function(){return o.a.createElement(o.a.Fragment,null,o.a.createElement(r.a,{title:"CV"}),"[Coming soon]")}}}]);
+//# sourceMappingURL=component---src-pages-cv-js-537752ac83005e1915de.js.map
---
+++ pyodideWorker.js
@@ -0,0 +1,69 @@
+const BASE_URL = "https://cdn.jsdelivr.net/pyodide/v0.15.0/full/"
+self.languagePluginUrl = BASE_URL
+importScripts(`${BASE_URL}pyodide.js`)
+
+const files = {
+  demo: "./python/demo.py",
+}
+
+const defaultData = {}
+
+const loadingDefaultData = new Promise((resolve, reject) => {
+  const promises = Object.entries(files).map(async ([name, path]) => {
+    const response = await fetch(path)
+    const content = await response.text()
+    defaultData[name] = content
+  })
+
+  Promise.all(promises).then(() => {
+    console.log("Python files loaded")
+    resolve(true)
+  })
+})
+
+var onmessage = async event => {
+  await languagePluginLoader
+  await loadingDefaultData
+
+  const { data } = event
+
+  switch (data.method) {
+    case "runPython":
+      pyodide.runPythonAsync(data.data).then(self.postMessage)
+      break
+    case "loadPackages":
+      await pyodide.loadPackage(data.data)
+      break
+    case "pyimport":
+      self.postMessage(pyodide.pyimport(data.data))
+      break
+    case "attachGlobal":
+      const globals = { ...defaultData, ...data.data }
+      Object.entries(globals).map(([key, value]) => {
+        if (key !== "__main__") self[key] = value
+      })
+      break
+    case "reload":
+      const i = new Promise((resolve, reject) => {
+        const promises = Object.entries(files).map(async ([name, path]) => {
+          const response = await fetch(path)
+          const content = await response.text()
+          defaultData[name] = content
+        })
+
+        Promise.all(promises).then(() => {
+          console.log("Python files loaded")
+          resolve(true)
+        })
+      })
+
+      await i
+
+      Object.entries({ ...defaultData, ...data.data }).map(([key, value]) => {
+        if (key !== "__main__") self[key] = value
+      })
+      break
+    default:
+      throw new Error("Invalid method called.")
+  }
+}
