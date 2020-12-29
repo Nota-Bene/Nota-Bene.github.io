d25f8f9954387f64059787bba0a4ae733a95c332
nina-werklaptop <n.becx@flexpulse.nl>
2020-12-29 20:28:37 +0000

    Updates

--- python/demo.py
+++ python/demo.py
@@ -1,6 +1,12 @@
+import time
+import random
+
 def parse(input):
   tokens = input.split(" ")
   parsedTokens = []
+  time.sleep(5)
+  for i in range(200000):
+    test = random.randint(1, 8) + random.randint(-4, 90)
   for token in tokens:
     if token == "":
       continue
@@ -8,6 +14,7 @@ def parse(input):
       "text": token,
       "lemma": token,
       "pos": "verb",
+      "decl": "3rd person singular future tense",
       "gloss": ["a test definition", "a second test definition"]
     })
   return parsedTokens
